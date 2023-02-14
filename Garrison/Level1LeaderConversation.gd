e47c66aaac1ff421f027374fb1519bd1274a7e65
Christopher R. Palmer <crpalmer@gmail.com>
2023-02-14 11:17:47 -0500

    Garrison Leader: Get rid of the complex code, push it down

--- Garrison/Level1LeaderConversation.gd
+++ Garrison/Level1LeaderConversation.gd
@@ -29,25 +29,23 @@ func player_said(_text, words):
 	if (words.has("walk") or words.has("walked")) and words.has("bar"):
 		say_bye("Funny joke.  But, now you must die.", 1)
 		GameEngine.player.add_xp(50)
-		call_deferred("spawn_guards", 1)
+		spawn_guards(1)
 	elif words.has("inspection") or words.has("inspect"):
 		say_bye("Oh, Vintar sent you?  Please look around, you'll find everything is in good shape.", 1)
 		GameEngine.player.add_xp(100)
 	elif one_word_in(words, [ "wander", "wandered", "accident", "accidentally", "mistake", "lost"]):
 		say_bye("Seriously?   You expect me to believe that crazy story??")
-		call_deferred("spawn_guards", 3)
+		spawn_guards(3)
+	elif words.has("bye"):
+		say_bye("Now, that's just rude.  Not even being civil and answering a question?  Guards!")
+		spawn_guards(4)
 	else:
 		say("That doesn't sound entirely believable.")
 		bad_answers += 1
 		if bad_answers >= 5:
 			say_bye("I think you're trying to trick me.  Guards!", 1)
-			call_deferred("spawn_guards", 2)
+			spawn_guards(2)
 
 func spawn_guards(n):
 	transport.teleport(actor)
-	for _i in range(n):
-		var guard = GameEngine.instantiate(GameEngine.current_scene, "res://DandD/Monsters/Kobold.tscn")
-		guard.set_physics_process(false)
-		yield(get_tree(), "idle_frame")
-		guard.place_near_player()
-		guard.set_physics_process(true)
+	GameEngine.spawn_near_player("res://DandD/Monsters/Kobold.tscn", n)
